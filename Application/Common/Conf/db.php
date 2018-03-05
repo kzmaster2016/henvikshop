7d56d8c84fdbacb11fb2c9673915998da8a1c3ce
e <1274599189@qq.com>
2018-03-05 18:11:17 +0800

    修改gitignore,数据库配置

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -3,7 +3,7 @@
 return array(
     'DB_TYPE' => 'mysql', //数据库类型
     'DB_HOST' => 'localhost', //数据库主机
-    'DB_NAME' => 'tpshop', //数据库名称
+    'DB_NAME' => 'henvikshop', //数据库名称
     'DB_USER' => 'root', //数据库用户名
     'DB_PWD' => 'admin', //数据库密码
     'DB_PORT' => '3306', //数据库端口
